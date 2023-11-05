261a9e4563b14529486567278a3439ef8de9edde
QuackGameDev <107073102+QuackGameDev@users.noreply.github.com>
2023-11-05 16:57:19 -0500

    Addition of Mongoose and Connection

--- memory-backend-nest/src/main.ts
+++ memory-backend-nest/src/main.ts
@@ -1,5 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as mongoose from 'mongoose';
+
+const url = `mongodb+srv://MemoriesUser42:RYplRI3sbvyjHTNi@memoriescluster.tyrwq3x.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(url)
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
