b5e1b7188d57a377cb1afc91aee9312f4d795e86
mickannor <89411564+mickannor@users.noreply.github.com>
2023-11-05 15:53:30 -0500

    email and memory creation

--- memories_back_end/src/email/email.service.js
+++ memories_back_end/src/email/email.service.js
@@ -4,10 +4,13 @@ export class EmailService {
     
     async create(email) {
         const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-        if email.value.match(validRegex) {
-
+        if (email.value.match(validRegex)) {
+            const createdEmail = new this.emailModel({
+                email: email
+            });
+            await createdEmail.save();
         } else {
-            throw 
+            pass;
         }
     }
 }
