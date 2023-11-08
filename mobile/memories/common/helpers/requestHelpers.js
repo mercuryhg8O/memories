225202ae9bffc6c9138253d852bf1fb6acd86d69
Matthew Merritt <merrittlmatt@gmail.com>
2023-11-08 14:15:23 -0500

    Fixed redundant declaration for getUserData

--- mobile/memories/common/helpers/requestHelpers.js
+++ mobile/memories/common/helpers/requestHelpers.js
@@ -21,40 +21,12 @@ const isValidUser = async (email, password) => {
     return true;
 }
 
-const getUserData = async (userid) => {
-  const query_string = `/api/userid?userid=${userid}`
-  const request_address = endpointURL + query_string
-  console.log('request made to: ' + request_address)
-
-  const response = await axios.get(request_address).catch((err) => {
-    console.log('error during retrieval response: ', err);
-    return false;
-  });
-
-  let username = 'a';
-  let bio = 'a';
-
-  if(response){
-    username = response.data.username;
-    bio = response.data.bio;
-  }
-  
-  return { userid, username, bio };
-}
-
-export {isValidUser, createUserSuccessful, getUserData};
-
 const createUserSuccessful = async (username, email, password, bio) => {
   const query_string = '/api/createaccount'
   const query_string1 = `/api/createaccount?email=${email}&password=${password}&username=${username}&bio=${bio}`
   const request_address = endpointURL + query_string
   console.log('request made to: ' + request_address)
   
-  // const response = await axios.get(request_address).catch((err) => {
-  //   // console.log('error during retrieval of when getting response: ', err);
-  //   return false;
-  // });
-
   const response = await axios.post(request_address,{
     username: username,
     email: email,
